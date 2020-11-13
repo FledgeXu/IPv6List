b14598925cafc5af56c151e4b6d321e6191d9898
FledgeXu <xzk0701@gmail.com>
2020-11-13 20:30:26 +0800

    Change way to check ipv6 support.

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm websites_with_ipv6.txt
 for i in $(cat ./websites.txt); do
-    #if ! dig AAAA ${i} @114.114.114.114 | grep "SOA"; then
-    if curl -H 'accept: application/dns-json' 'https://cloudflare-dns.com/dns-query?name='${i}'&type=AAAA' | jq -e .Answer; then
+    #if curl -H 'accept: application/dns-json' 'https://cloudflare-dns.com/dns-query?name='${i}'&type=AAAA' | jq -e .Answer; then
+    if ! dig AAAA ${i} @1.1.1.1 | grep "SOA"; then
         echo ${i} >> websites_with_ipv6.txt 
     fi
 done
